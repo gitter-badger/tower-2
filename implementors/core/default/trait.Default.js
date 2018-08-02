c4d6340ea394026149e093f65d656282561ff34c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-02 20:18:11 +0000

    Deploy tower-rs/tower to github.com/tower-rs/tower.git:gh-pages

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["tower_balance"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"tower_balance/choose/struct.PowerOfTwoChoices.html\" title=\"struct tower_balance::choose::PowerOfTwoChoices\">PowerOfTwoChoices</a>",synthetic:false,types:["tower_balance::choose::p2c::PowerOfTwoChoices"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"tower_balance/choose/struct.RoundRobin.html\" title=\"struct tower_balance::choose::RoundRobin\">RoundRobin</a>",synthetic:false,types:["tower_balance::choose::round_robin::RoundRobin"]},{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"tower_balance/load/pending_requests/struct.Count.html\" title=\"struct tower_balance::load::pending_requests::Count\">Count</a>",synthetic:false,types:["tower_balance::load::pending_requests::Count"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
